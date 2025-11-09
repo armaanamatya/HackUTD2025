0d2ab213f4cd489f068f8b271e9b805b134a6d69
rishi-golla <rishigolla.123@gmail.com>
2025-11-09 02:06:57 -0600

    frontpage skeleton

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Montserrat } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ 
   subsets: ['latin'],
   variable: '--font-inter',
   display: 'swap',
+})
+
+const cbre = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-cbre',
+  display: 'swap',
+  weight: ['400', '500', '600', '700'],
 })
 
 export const metadata: Metadata = {
@@ -25,7 +32,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className={`${inter.variable} font-sans antialiased`}>
+      <body className={`${inter.variable} ${cbre.variable} font-sans antialiased`}>
         {children}
       </body>
     </html>
